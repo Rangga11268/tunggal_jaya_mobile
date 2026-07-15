e0adbf34b83185a28af4e76418cc98ca8d012ff9
Darell Rangga <darrelrangga@gmail.com>
2026-07-16 05:53:57 +0700

    Enable mouse dragging for ScrollView to allow swiping cards with mouse

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter/gestures.dart';
 import 'app.dart';
 import 'core/config/app_theme.dart';
+
+class CustomScrollBehavior extends MaterialScrollBehavior {
+  @override
+  Set<PointerDeviceKind> get dragDevices => {
+        PointerDeviceKind.touch,
+        PointerDeviceKind.mouse,
+        PointerDeviceKind.stylus,
+        PointerDeviceKind.trackpad,
+      };
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,6 +43,7 @@ class TunggalJayaApp extends ConsumerWidget {
       title: 'Tunggal Jaya Transport',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
+      scrollBehavior: CustomScrollBehavior(),
       routerConfig: router,
     );
   }
